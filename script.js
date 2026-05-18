c665b4ab01027a8ce26657e9e4cc1f2ea1fefb14
Loveisdancing <tothemjb@gmail.com>
2026-05-18 14:32:51 -0400

    iam... experience upload

--- script.js
+++ script.js
@@ -9,3 +9,29 @@ star.addEventListener('click', function () {
         star.textContent = '✦';
     }
 })
+
+  const fonts = [
+  "Inter Tight",
+  "Jacquard 12 Charted",
+  "Jersey 10",
+  "Micro 5 Charted",
+  "Pixelify Sans",
+  "Rubik Broken Fax",
+  "Silkscreen",
+  "Syne Mono",
+  "Doto"
+];
+
+const button = document.querySelector(".pword button");
+const buttonText = button.textContent;
+button.textContent = "";
+
+[...buttonText].forEach(char => {
+  const buttonSpan = document.createElement("span");
+  buttonSpan.textContent = char;
+
+  const randomButtonFont = fonts[Math.floor(Math.random() * fonts.length)];
+  buttonSpan.style.fontFamily = randomButtonFont;
+
+  button.appendChild(buttonSpan);
+});
