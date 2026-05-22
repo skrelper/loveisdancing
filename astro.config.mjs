c234bd103d5240a845dcb65b1ce255fe274be7c3
Loveisdancing <tothemjb@gmail.com>
2026-05-21 20:03:52 -0400

    hugeee update just adding pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,11 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   redirects: {
-    '/prompts': '/iam/prompts'
+    '/prompts': '/iam/prompts',
+    '/music': '/files/music',
+    '/gallery': '/files/gallery',
+    '/blog': '/files/blog',
+    '/store': '/files/store',
+    '/archive': '/files/archive'
   }
 });
